6670577228538654af52c75365ba28cc7f556ec2
Bhavya Grover <bhavya2526@gmail.com>
2023-07-30 00:57:02 +0530

    navbar changes

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,24 +1,58 @@
-const body = document.querySelector("body");
-const sidebar = document.querySelector("nav");
-const toggle = document.querySelector(".toggle");
-const searchBox = document.querySelector(".search-box");
-const modeSwitch = document.querySelector(".toggle-switch");
-const modeText = document.querySelector(".mode-text");
 
-toggle.addEventListener("click", () => {
-    console.log("Toggle clicked");
-    sidebar.classList.toggle("close");
-});
+const searchButtonMobile = document.getElementById("search-button-mobile");
+const searchBar = document.getElementById("search-bar");
+const menuButton = document.getElementById("menu-button");
+const navLinksContainer = document.getElementById("nav-links");
+const navLinks = document.getElementsByClassName("nav-link");
 
-searchBox.addEventListener("click", () => {
-    console.log("Search box clicked");
-    sidebar.classList.remove("close");
-});
+//show/hide search bar
+searchButtonMobile.addEventListener("click", (event) => {
+  event.stopPropagation();
+  if (searchBar.style.display === 'none') {
+    searchBar.style.display = 'flex';
+    searchButtonMobile.style.display = "none";
+  } else {
+    searchBar.style.display = 'none';
+    searchButtonMobile.style.display = "block";
+  }
+})
 
-modeSwitch.addEventListener("click", () => {
-    console.log("Mode switch clicked");
-    body.classList.toggle("dark");
-    modeText.innerText = body.classList.contains("dark")
-        ? "Light Mode"
-        : "Dark Mode";
-});
+//show/hide menu
+menuButton.addEventListener("click", (event) => {
+  event.stopPropagation();
+
+  if(navLinksContainer.style.display === "block") {
+    navLinksContainer.style.display = "none";
+  } else {
+    navLinksContainer.style.display = "block";
+  }
+})
+
+//hide menu and searchbar if click anywhere on screen
+document.addEventListener("click", (event) => {
+  if(event.target !== searchBar && !searchBar.contains(event.target) && window.innerWidth < 799) {
+    searchBar.style.display = "none";
+    searchButtonMobile.style.display = "block";
+  }
+  if(event.target !== navLinksContainer && !navLinksContainer.contains(event.target) && window.innerWidth < 799) {
+    navLinksContainer.style.display = "none"; 
+  }
+})
+
+
+window.addEventListener("resize", ()=> {
+  searchBar.removeAttribute("style");
+  navLinksContainer.removeAttribute("style");
+})
+
+//add active class to clicked link
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", function() {
+    // Remove "active" class from all links
+    for (let j = 0; j < navLinks.length; j++) {
+      navLinks[j].classList.remove("active");
+    }
+    // Add "active" class to the clicked link
+    this.classList.add("active");
+  });
+}
