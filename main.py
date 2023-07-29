094a0394dbf9371a0381a314917bb81627ccb006
Bhavya Grover <bhavya2526@gmail.com>
2023-07-29 23:37:58 +0530

    search allergy

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import os
 import requests
-from flask import Flask, render_template, request
+import pickle
+import pandas as pd
+from flask import Flask, render_template, request , jsonify
 from jinja2 import Template
+from py_edamam import PyEdamam 
 
 app = Flask(__name__, static_folder='static')
 app.config["DEBUG"] = True
 
 os.environ['EDAMAM_APP_ID'] = '4ba2acf1'
 os.environ['EDAMAM_APP_KEY'] = '984e714dc4b8dee2ecf753e1b72dec20'
-
 
 url = "https://api.humanapi.co/v1/human/medical/allergies"
 
@@ -21,14 +23,83 @@ response = requests.get(url, headers=headers)
 
 print(response.text)
 
+data = pd.read_csv('C:/Bhavya/Projects/Allergy Tracker/FoodData.csv')
+
+# Define the Node class for the linked list
+class Node:
+    def __init__(self, food, allergy):
+        self.food = food
+        self.allergy = allergy
+        self.next = None
+
+# Define the LinkedList class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, food, allergy):
+        new_node = Node(food, allergy)
+        new_node.next = self.head
+        self.head = new_node
+
+    def search(self, food):
+        current_node = self.head
+        while current_node:
+            if current_node.food.lower() == food.lower():  # Case-insensitive comparison
+                return current_node.allergy
+            current_node = current_node.next
+        return "No allergy information available"
+
+# Function to read CSV and create the linked list
+def read_csv_and_create_linked_list(csv_file):
+    linked_list = LinkedList()
+    with open(csv_file, 'r') as file:
+        lines = file.readlines()
+        for line in lines[1:]:  # Skip the header line
+            class_, type_, group, food, allergy = line.strip().split(',')
+            linked_list.insert(food, allergy)
+    return linked_list
+
+def save_linked_list_to_file(linked_list, file_name):
+    with open(file_name, 'wb') as file:
+        pickle.dump(linked_list, file)
+
+# Load the linked list from the saved file
+def load_linked_list_from_file(file_name):
+    with open(file_name, 'rb') as file:
+        linked_list = pickle.load(file)
+    return linked_list
+
+@app.route('/index')
+def display_linked_list():
+    linked_list = load_linked_list_from_file('linked_list_data.pickle')
+
+    # Convert the linked list data to a pandas DataFrame
+    data = []
+    current_node = linked_list.head
+    item_number = 1
+    while current_node:
+        data.append([item_number, current_node.food, current_node.allergy])
+        current_node = current_node.next
+        item_number += 1
+
+    df = pd.DataFrame(data, columns=['Item Number', 'Food', 'Allergy'])
+
+    # Convert the DataFrame to an HTML table
+    table_html = df.to_html(index=False)
+
+    return render_template('index.html', table_html=table_html)
+
+
 @app.route('/')
 def navbar():
-    return render_template('navbar.html')
+    return render_template('dashboard.html')
 
 @app.route('/recipes')
 def search():
     ingredient = request.args.get('ingredient')
 
+    # Call the edamam_search function to get the list of recipes
     hits = edamam_search(ingredient)
 
     return render_template('recipes.html', ingredient=ingredient, hits=hits)
@@ -46,10 +117,35 @@ def edamam_search(query):
     if response.status_code == 200:
         data = response.json()
         hits = data.get('hits', [])
-    else:
-        hits = []  # Return an empty list if the API request failed
+        # Extract the list of ingredients for each recipe
+        recipes_with_allergies = []
+        linked_list = load_linked_list_from_file('linked_list_data.pickle')
+        for hit in hits:
+            recipe = hit.get('recipe', {})
+            label = recipe.get('label', '')
+            image = recipe.get('image', '')
+            url = recipe.get('url', '')
+            ingredients = recipe.get('ingredientLines', [])
+            matched_ingredients_with_allergies = []
+            for ingredient in ingredients:
+                words = ingredient.split()  # Split ingredient into individual words
+                matching_words = [word for word in words if linked_list.search(word) != "No allergy information available"]
+                if matching_words:
+                    matched_ingredient = " ".join(matching_words)
+                    allergy = linked_list.search(matched_ingredient)
+                    matched_ingredients_with_allergies.append({'ingredient': matched_ingredient, 'allergy': allergy})
+            if matched_ingredients_with_allergies:
+                recipes_with_allergies.append({'label': label, 'image': image, 'url': url, 'ingredients_with_allergies': matched_ingredients_with_allergies})
 
-    return hits
+    return recipes_with_allergies
+
+@app.route('/allergy')
+def allergy():
+    return data.head().to_string()
 
 if __name__ == '__main__':
+    csv_file = 'FoodData.csv'
+    linked_list = read_csv_and_create_linked_list(csv_file)
+    save_linked_list_to_file(linked_list, 'linked_list_data.pickle')
     app.run()
+
